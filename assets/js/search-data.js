3ffa8d02328d70b6e7a929819f6fbfc64604a931
HyunjooHo <ho274@purdue.edu>
2026-04-10 05:29:03 +0000

    Deploying to gh-pages from @ HyunjooHo/hyunjooho.github.io@e2140c509b788ca7d03429ff9264be6e68166e0f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -28,9 +28,4 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "teachings-introduction-to-machine-learning",
-          title: 'Introduction to Machine Learning',
-          description: "This course provides an introduction to machine learning concepts, algorithms, and applications. Students will learn about supervised and unsupervised learning, model evaluation, and practical implementations.",
-          section: "Teachings",handler: () => {
-              window.location.href = "/teachings/introduction-to-machine-learning/";
             },},];
